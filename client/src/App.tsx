cb471b89d676ba869f1cc42fbb2938fc068bf2be
Izzatbek <abdusharipovizzat03@gmail.com>
2026-05-10 14:32:37 +0500

    Open Weather API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import {  useState, type KeyboardEvent } from "react";
 import type WeatherDto from "./interface/weather.interface";
 import axios from "axios";
+import {toast} from "react-toastify";
 
 const App = () =>{
     const [weather,setWeather]=useState<WeatherDto>();
@@ -12,6 +13,7 @@ const App = () =>{
             const {data} = await axios.get(`http://localhost:8080/api/v1/weather?q=${search}`);
             setWeather(data);
         } catch (error) {
+            toast.error("Something went wrong!");
             console.log(error);
         }
     }
@@ -25,7 +27,7 @@ const App = () =>{
     return (
         <>
            <div className={"w-full h-screen flexing"}>
-               <div className={"w-full max-w-3xl mx-auto shadow-lg text-center"}>
+               <div className={"w-full max-w-3xl mx-auto shadow-md text-center border border-blue-200 rounded-md p-4 hover:shadow-lg transition duration-300"}>
                    <h2 className={"text-3xl font-extrabold text-blue-600"}>{weather?.name ?? "Your City"} <span className="text-blue-900">({weather?.sys.country ?? "UZ"})</span></h2>
                    <h1 className={"text-6xl font-extrabold py-6"}>{weather?.main.temp ?? 0}°C</h1>
                    <div className="space-y-4 px-6">
@@ -40,7 +42,8 @@ const App = () =>{
                     </div>
                    </div>
                    <div className={"w-full p-5"}>
-                       <div className={"flex items-center gap-2 border border-blue-200 p-2 rounded-md"}>
+                       <div className={"flex items-center gap-2 border border-blue-200 p-1 md:p-2 rounded-md" +
+                           "shadow-md hover:shadow-lg transition duration-300 rounded-md"}>
                            <input type={"text"} 
                            placeholder={"Enter your city or country...."}
                             className={"outline-none w-full bg-transparent"}
@@ -51,7 +54,7 @@ const App = () =>{
                            <button
                                type={"submit"}
                                onClick={getWeather}
-                               className={"bg-blue-500 text-white px-8 text-sm py-4 rounded-md"}>Search</button>
+                               className={"bg-blue-500 text-white px-8 text-sm py-2 md:py-4 rounded-md hover:shadow-lg transition duration-300"}>Search</button>
                        </div>
                    </div>
                </div>
